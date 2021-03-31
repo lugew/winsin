b1525e547b535f4b9902696d6cb1af67e0fbed68
xialugui <xialugui@mimovr.com>
2021-03-31 14:38:19 +0800

    feature:
    1. 增加Curd控制器

--- web/src/main/java/com/lugew/winsin/web/AbstractController.java
+++ web/src/main/java/com/lugew/winsin/web/AbstractController.java
@@ -5,13 +5,8 @@ import com.lugew.winsin.core.entity.Entity;
 import com.lugew.winsin.core.exception.Exception;
 import com.lugew.winsin.core.service.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import java.io.Serializable;
-import java.util.List;
 
 /**
  * @author 夏露桂
@@ -22,42 +17,10 @@ import java.util.List;
 public abstract class AbstractController<T extends Entity<ID>, S extends Service<T, ID>, ID extends Serializable> implements Controller<T, ID> {
     @Autowired
     @SuppressWarnings("ALL")
-    protected S service;
+    private S service;
 
-    @PostMapping({"/getList"})
-    public List<T> getList(@RequestBody T entity) {
-        return service.getList(entity);
-    }
-
-
-    @Override
-    @PostMapping("/getListPaging")
-    public Object getListPaging(@RequestBody T entity, @RequestParam Integer pageIndex, @RequestParam Integer pageSize) {
-        return service.getPagingList(entity, pageIndex, pageSize);
-    }
-
-    @Override
-    @GetMapping({"/get"})
-    public T get(ID id) {
-        return service.get(id);
-    }
-
-    @Override
-    @PostMapping({"/add"})
-    public boolean add(@RequestBody T entity) {
-        return service.insert(entity);
-    }
-
-    @Override
-    @PostMapping({"/update"})
-    public boolean update(@RequestBody T entity) {
-        return service.update(entity);
-    }
-
-    @Override
-    @GetMapping({"/delete"})
-    public boolean delete(ID id) {
-        return service.delete(id);
+    public S getService() {
+        return service;
     }
 
     public void throwException(String key, Object... arguments) {
---
+++ web/src/main/java/com/lugew/winsin/web/CrudController.java
@@ -0,0 +1,57 @@
+package com.lugew.winsin.web;
+
+import com.lugew.winsin.core.entity.Entity;
+import com.lugew.winsin.core.service.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.io.Serializable;
+import java.util.List;
+
+/**
+ * 增删改查控制器
+ *
+ * @author 夏露桂
+ * @since 2021/1/15 15:36
+ */
+public abstract class CrudController<T extends Entity<ID>, S extends Service<T, ID>, ID extends Serializable> extends AbstractController<T, S, ID> {
+
+    @PostMapping({"/getList"})
+    public List<T> getList(@RequestBody T entity) {
+        return getService().getList(entity);
+    }
+
+
+    @Override
+    @PostMapping("/getListPaging")
+    public Object getListPaging(@RequestBody T entity, @RequestParam Integer pageIndex, @RequestParam Integer pageSize) {
+        return getService().getPagingList(entity, pageIndex, pageSize);
+    }
+
+    @Override
+    @GetMapping({"/get"})
+    public T get(ID id) {
+        return getService().get(id);
+    }
+
+    @Override
+    @PostMapping({"/add"})
+    public boolean add(@RequestBody T entity) {
+        return getService().insert(entity);
+    }
+
+    @Override
+    @PostMapping({"/update"})
+    public boolean update(@RequestBody T entity) {
+        return getService().update(entity);
+    }
+
+    @Override
+    @GetMapping({"/delete"})
+    public boolean delete(ID id) {
+        return getService().delete(id);
+    }
+
+}
